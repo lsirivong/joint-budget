983a9f41644f62082d8f9fd5d53e6380501d57ec
Lenny Sirivong <lenny@lenny-s.com>
2014-07-23 08:06:41 +0700

    show purchaser in list.

--- app/helpers/line_items_helper.rb
+++ app/helpers/line_items_helper.rb
@@ -1,7 +1,15 @@
 module LineItemsHelper
 	def get_member_select_options
-    members = Member.all
-    @member_options = members.map { |m| [m.name, m.id] }
-    [['Select...', nil]].concat @member_options
-  end
+		members = Member.all
+		@member_options = members.map { |m| [m.name, m.id] }
+		[['Select...', nil]].concat @member_options
+	end
+
+	def get_purchaser_name(line_item)
+		unless line_item.member.nil?
+			return line_item.member.name
+		else
+			return ''
+		end
+	end
 end
--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,2 +1,7 @@
 module MembersHelper
+	def line_item_list_items(member)
+		h = ""
+		member.line_items.each { |l| h += "<li>#{l.description}: #{l.amount}</li>" }
+		return h
+	end
 end
