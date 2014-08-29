1378733d09662d8dd7322d705c1986b6808e0121
Lenny Sirivong <lenny@lenny-s.com>
2014-08-29 09:11:42 -0400

    add home page and nav.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def nav_item(text, options)
+		is_current = current_page?(options)
+		active_class = is_current ? "active" : "";
+		path = url_for options
+		"<li class=\"#{active_class}\"><a href=\"#{path}\">#{text}</a></li>".html_safe
+	end
 end
