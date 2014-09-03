a66336a36887a4a7657e12bf281c1e125021e0d7
Lenny Sirivong <lenny@lenny-s.com>
2014-09-03 17:06:59 -0400

    simplify normal member UX

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
 	def index
-		
+		unless member_signed_in?
+			redirect_to new_member_session_path
+		end
+
+		@budget = current_member.budgets.order('created_at DESC').first
 	end
 end
