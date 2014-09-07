a71e2468e20d506dad4d3c01943435b922415f79
Lenny Sirivong <lenny@lenny-s.com>
2014-09-07 17:32:53 -0400

    fix nil ref for users with no budgets.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,9 @@ class HomeController < ApplicationController
 			end
 
 			@line_items = LineItem.all.order('purchased_at DESC')
-			@line_item = LineItem.new(purchased_at: Date.today, member_id: current_member.id, budget_id: current_member.budgets.first.id);
+
+			default_budget_id = current_member.budgets.any? ? current_member.budgets.first.id : nil
+			@line_item = LineItem.new(purchased_at: Date.today, member_id: current_member.id, budget_id: default_budget_id);
 		else
 			redirect_to new_member_session_path
 		end
